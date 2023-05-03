d2d20955b798626e0db06b21b79c8c5af3844a8b
Mohammad <moh.hossain98@gmail.com>
2023-05-03 12:14:41 -0400

    lecture code

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -4,16 +4,29 @@
 # 2. ✅ Instantiate a few pet instance 
     # Compare the pet instances to demonstrate they are not the same object
     # Note: add 'pass' to the pet class 
-
-
+import ipdb
 class Pet:
-    # pass
+    count = 0 
 # 3. ✅ Demonstrate __init__ 
     # Add arguments to instances  
     # use dot notation to access their attributes 
     # update attributes with new values 
-
-
+    # properties of pets: 
+    # name
+    # species
+    # age
+    # size
+    # temperment
+    def __init__(self, name, species, age, size, temperment):
+        self.name = name
+        self.species = species
+        self.size = size
+        self.age = age
+        self.temperment = temperment
+        Pet.count += 1  
+        # instance variable: variable that belongs to the each instance of the class
+        # class variable: variable that belongs to the class
+        
 # 4.✅ Demonstrate instance methods by creating a print_pet_details function that will print the pet attributes
 #     Review the self keyword 
 #     Invoke the print_pet_details on an instance 
@@ -23,8 +36,20 @@ class Pet:
             # breed:domestic longhair
             # temperament:sweet
             # image_url:rose.jpg
-
-
+    def print_pet_details(self):
+        print(f'''
+              name: {self.name}
+              age: {self.age}
+              temperament: {self.temperment}
+              size: {self.size}
+              species: {self.species}
+              ''')
+    def get_details_in_one_line(self):
+        print(f"{self.name} is a {self.species}, who is {self.temperment} and {self.size} in size and {self.age} years old.")
+        
+    @classmethod
+    def get_pet_count(Pet): 
+        print(f"There are {Pet.count} pets")
 # Demonstrate instances 
     # Different Instances are Different Objects
 # Demonstrate __init__
@@ -48,3 +73,7 @@ class Pet:
 # rose.age = 12
 # rose.age -> 12
 
+spot = Pet("Spot", "Dog", 6, "medium", "Playful")
+rose = Pet("Rose", "Cat", 4, "small", "Crazy")
+
+ipdb.set_trace()
