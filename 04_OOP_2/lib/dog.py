943d84500ca7d9b896e1add33451c571aa98347f
Mohammad <moh.hossain98@gmail.com>
2023-05-04 12:12:28 -0400

    lecture

---
+++ 04_OOP_2/lib/dog.py
@@ -0,0 +1,22 @@
+from pet import Pet
+import ipdb
+
+
+class Dog(Pet): 
+    def __init__(self, name, age, breed, temperament, sound, loyalty, color):
+        super().__init__(name, age, breed, temperament)
+        self.sound = sound
+        self.loyalty = loyalty
+        self.color = color
+        
+    def talk(self):
+        print("Woof woof")
+        
+    def print_pet_details(self):
+        super().print_pet_details()
+        print(f'''              sound: {self.sound}
+            loyalty: {self.loyalty}
+            color: {self.color}
+            ''')
+        
+ipdb.set_trace()
--- 04_OOP_2/lib/pet.py
+++ 04_OOP_2/lib/pet.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 # Class Attributes and Methods 
 
+# import ipdb;
 class Pet:
+    
+    count = 0
 
-    def __init__(self,name, age, breed, temperament, image_url):
+    def __init__(self,name, age, breed, temperament):
         self.name = name
         self.age = age
         self.breed = breed
         self.temperament = temperament
-        self.image_url = image_url
+        Pet.count += 1
  
     
-    # 6✅. Create a class method increase_pets that will increment total_pets
-        # replace Pet.total_pets += 1 in __init__ with increase_pets()
+    # 6✅. Create a class method that will print the pet count
+    @classmethod
+    def pet_count(cls): 
+        print(f"The number of pets: {cls.count}")
 
     def print_pet_details(self):
         print(f'''
             name:{self.name}
             age:{self.age}
             breed:{self.breed}
-            temperament:{self.temperament}
-            image_url:{self.image_url}
-        ''')
+            temperament:{self.temperament}''')
+        
+# ipdb.set_trace()
 
 
 
